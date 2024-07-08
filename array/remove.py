792c3a0490144c6177c9d2daf66a771086fd4431
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-08 12:33:12 +0530

    [vinit] add:program to find count of target element

---
+++ array/remove.py
@@ -0,0 +1,13 @@
+number=int(input("Enter the length of array: "))
+
+arr=[]
+for i in range(number):
+    arr.append(int(input(f"Enter the element at position {i+1}: ")))
+target=int(input("Enter the element to delete from the list: "))
+
+for i in arr:
+    if i == target:
+        arr.remove(target)
+        break
+
+print(arr)
