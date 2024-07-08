3d5450c5d3ffc75c8ffd5d59c5015309c8d5ed90
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-09 01:36:25 +0530

    [vinit] add:program to create frozen set

---
+++ sets/frozenset.py
@@ -0,0 +1,8 @@
+frozenset1 = frozenset([1, 2, 3, 4, 5])
+frozenset2 = frozenset([4, 5, 6, 7, 8])
+
+# Displaying the frozensets
+print("Frozenset 1:", frozenset1)
+#frozenset1.add(9)
+print("Frozenset 2:", frozenset1)
+
