d229aa22fb414d233a18abe19ccf136dd55fffe1
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-09 01:38:54 +0530

    [vinit] add:program to find maximum and minimum value in set

---
+++ sets/max_and_min.py
@@ -0,0 +1,10 @@
+sample_set = {5, 1, 8, 3, 9, 2}
+
+
+max_value = max(sample_set)
+
+min_value = min(sample_set)
+
+
+print("The maximum value in the set is:", max_value)
+print("The minimum value in the set is:", min_value)
