cb2772867e8b41b1278f9c0905afd7bbaa383f0e
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-09 18:15:00 +0530

    [vinit] add:program to find length of string

---
+++ string/len.py
@@ -0,0 +1,9 @@
+string="Vinit Bhamare"
+
+l1=len(string)
+
+count=0
+for i in string:
+    count+=1
+print(f"length of string using 'len' function : {l1}")
+print(f"length of string using 'len' function : {count}")
