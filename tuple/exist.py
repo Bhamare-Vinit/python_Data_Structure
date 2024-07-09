2d1ac49c68b746a6c0d3d69db30f1b3ac229fb38
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-09 17:52:52 +0530

    [vinit] add:program to check if target element is present in tuple

---
+++ tuple/exist.py
@@ -0,0 +1,6 @@
+target=input(f"Enter the element you want to find in tuple: ")
+fruits = ("apple", "banana", "cherry","apple")
+
+if target in fruits:
+    print(f"{target} element is present in tuple: {fruits}")
+
